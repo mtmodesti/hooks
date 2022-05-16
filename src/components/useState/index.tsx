629bf68bf3530567b37f3aa0e10781367d3dc39f
mtmodesti <marcelomodesti1988@gmail.com>
2022-05-16 08:46:51 -0300

    feat: useState componnent refatored with context api

--- src/components/useState/index.tsx
+++ src/components/useState/index.tsx
@@ -1,76 +1,77 @@
 import "./styles.css";
 import { useState } from "react";
 import { useContext } from "react";
-import { CounterProvider } from "../../providers/counterProvider/counter";
 import { CounterContext } from "../../providers/counterProvider/counter";
 
 export const UseStateComponent = () => {
-  const [count, setCount] = useState(0);
-  const teste = "const somar = () => useState(count + 1)";
 
   const counter = useContext(CounterContext);
-  
-
+  const [count, setCount] = useState(counter);
   
   return (
     <main>
-      <p>
-        Nosso principal e mais básico hook do React.JS. Sua função é a
-        capacidade de armazenar um valor, esse valor pode ser de qualquer tipo,
-        uma string, um array, um booleano, um array de objetos, enfim, o que
-        você precisar armazenar, e, além de armazenar, podemos alterar sempre
-        que necessário esse valor. Vamos entender um pouco melhor. Para declarar
-        uma variável com o useState, usamos a seguinte sintaxe: {<br />}{" "}
-        {<br />}
-        const [count, setCount] = useState(0){<br />}
-        {<br />}
-        Perceba que estamos desestruturando de dentro do useState um array com
-        dois valores, o count e o setCount, e logo em frente atribuindo o valor
-        inicial de 0. Os nomes poderiam ser qualquer coisa, mas vamos usar o
-        famoso exemplo do contador. Estamos iniciando um variável qualquer com o
-        nome de count e atribuindo para ela o valor de 0.{<br />}
-        {<br />}
-        Para alterarmos agora o seu valor, usamos o setCount, que
-        desestruturamos do useState e nada mais que é que uma função do react
-        com a capacidade de alterar o valor do count. Não podemos alterar
-        diretamente igual no JavaScript pois o React trabalha com o conceito de
-        imutabilidade, tópico para outro dia. A padronização dos nomes segue
-        essa estrutura, declaramos uma variável primeira para armazenar o valor,
-        e função de alterar a variável leva o nome set como prefixo:
-        {<br />}
-        {<br />}
-        Exemplos: [lista, setLista] ou [arrayQualquer, setArrayQualquer]{" "}
-        {<br />}
-        {<br />}
-        Então agora que entedemos como salvar um valor, e que precisamos usar o
-        segundo parâmetro para alterar o valor ficou fácil montarmos um simples
-        contador de exemplo. Basta criarmos um botão para executar a função de
-        somar e um outro para diminuir o valor do contador, que inicialmente
-        definimos como 0 por padrão. Para a função de somar, usamos o seguinte
-        trecho: <br />
-        {<br />}
-        `${teste}`<br />
-        {<br />}E a mesma coisa para a função de diminuir, só que com count - 1.
-        Agora basta inserirmos isso na função de onClick de nosso botão e
-        inserir tudo isso para renderizar em tela. O resultado vc confere abaixo
-      </p>
-      <div className="counter">
-        <button
-          onClick={() => {
-            setCount(count - 1);
-          }}
-        >
-          diminuir
-        </button>
-        <span>{count}</span>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          Somar
-        </button>
-      </div>
+      <CounterContext.Provider value={{ count, setCount }}>
+        <button onClick={() => {}}></button>
+        <p>
+          Nosso principal e mais básico hook do React.JS. Sua função é a
+          capacidade de armazenar um valor, esse valor pode ser de qualquer
+          tipo, uma string, um array, um booleano, um array de objetos, enfim, o
+          que você precisar armazenar, e, além de armazenar, podemos alterar
+          sempre que necessário esse valor. Vamos entender um pouco melhor. Para
+          declarar uma variável com o useState, usamos a seguinte sintaxe:{" "}
+          {<br />} {<br />}
+          const [count, setCount] = useState(0){<br />}
+          {<br />}
+          Perceba que estamos desestruturando de dentro do useState um array com
+          dois valores, o count e o setCount, e logo em frente atribuindo o
+          valor inicial de 0. Os nomes poderiam ser qualquer coisa, mas vamos
+          usar o famoso exemplo do contador. Estamos iniciando um variável
+          qualquer com o nome de count e atribuindo para ela o valor de 0.
+          {<br />}
+          {<br />}
+          Para alterarmos agora o seu valor, usamos o setCount, que
+          desestruturamos do useState e nada mais que é que uma função do react
+          com a capacidade de alterar o valor do count. Não podemos alterar
+          diretamente igual no JavaScript pois o React trabalha com o conceito
+          de imutabilidade, tópico para outro dia. A padronização dos nomes
+          segue essa estrutura, declaramos uma variável primeira para armazenar
+          o valor, e função de alterar a variável leva o nome set como prefixo:
+          {<br />}
+          {<br />}
+          Exemplos: [lista, setLista] ou [arrayQualquer, setArrayQualquer]{" "}
+          {<br />}
+          {<br />}
+          Então agora que entedemos como salvar um valor, e que precisamos usar
+          o segundo parâmetro para alterar o valor ficou fácil montarmos um
+          simples contador de exemplo. Basta criarmos um botão para executar a
+          função de somar e um outro para diminuir o valor do contador, que
+          inicialmente definimos como 0 por padrão. Para a função de somar,
+          usamos o seguinte trecho: <br />
+          {<br />}
+          `${counter}`<br />
+          {<br />}E a mesma coisa para a função de diminuir, só que com count -
+          1. Agora basta inserirmos isso na função de onClick de nosso botão e
+          inserir tudo isso para renderizar em tela. O resultado vc confere
+          abaixo
+        </p>
+        <div className="counter">
+          <button
+            onClick={() => {
+              setCount(count - 1);
+            }}
+          >
+            diminuir
+          </button>
+          <span>{count}</span>
+          <button
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            Somar
+          </button>
+        </div>
+      </CounterContext.Provider>
     </main>
   );
 };
--- src/providers/counterProvider/counter.tsx
+++ src/providers/counterProvider/counter.tsx
@@ -1,9 +1,14 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
 
 
 export const CounterContext = createContext(0);
 
-export const CounterProvider = ({ children}:any) => {
+export const CounterProvider = ({ children }: ProvidersProps) => {
   let [counter, setCounter] = useState(0);
 
   const plusOne = () => {
@@ -14,9 +19,9 @@ export const CounterProvider = ({ children}:any) => {
     setCounter(counter - 1);
   };
 
-    return (
-      <CounterContext.Provider value={{ minusOne , plusOne}}>
-        {children}
-      </CounterContext.Provider>
-    );
+  return (
+    <CounterContext.Provider value={{ minusOne, plusOne }}>
+      {children}
+    </CounterContext.Provider>
+  );
 };
